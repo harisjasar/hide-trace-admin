9abfb26a24856d267409db556858470d4a556296
harisjasar <hare.jasarevic@gmail.com>
2020-02-27 03:44:10 +0100

    added Certificate & HideType model beans

---
+++ src/main/java/com/hidetrace/admin/model/HideTypeModel.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hidetrace.admin.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Jashar
+ */
+@Component
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "certificate")
+public class HideTypeModel {
+
+    @Id
+    @Column(name = "CertificateID")
+    private int hideTypeId;
+    @Column(name = "Name")
+    private String name;
+    @Column(name = "Description")
+    private String description;
+}
