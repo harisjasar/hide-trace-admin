19e3f46a32ee00e23ab7e08295aceef22869bd60
harisjasar <hare.jasarevic@gmail.com>
2020-02-27 03:41:35 +0100

    added CertificateService bean

---
+++ src/main/java/com/hidetrace/admin/service/CertificateService.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hidetrace.admin.service;
+
+import com.hidetrace.admin.model.CertificateModel;
+import com.hidetrace.admin.repository.incominginvoice.CertificateRepository;
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Jashar
+ */
+@Service
+public class CertificateService {
+
+    @Autowired
+    private CertificateRepository repo;
+
+    @Transactional
+    public List<CertificateModel> findAll() {
+        return repo.findAll();
+    }
+}
