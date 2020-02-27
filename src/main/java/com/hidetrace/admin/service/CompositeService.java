da633616ce304679e38966134a05ab0da794e2b9
harisjasar <hare.jasarevic@gmail.com>
2020-02-27 08:00:06 +0100

    added composite service

---
+++ src/main/java/com/hidetrace/admin/service/CompositeService.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hidetrace.admin.service;
+
+import com.hidetrace.admin.model.incominginvoice.IncomingInvoiceCertificateModel;
+import com.hidetrace.admin.model.incominginvoice.IncomingInvoiceHideTypeModel;
+import com.hidetrace.admin.model.incominginvoice.IncomingLegalEntityInvoiceModel;
+import com.hidetrace.admin.service.incominginvoice.IncomingInvoiceCertificateService;
+import com.hidetrace.admin.service.incominginvoice.IncomingInvoiceHideTypeService;
+import com.hidetrace.admin.service.incominginvoice.IncomingLegalEntityInvoiceService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author Jashar
+ */
+@Component
+@Transactional
+public class CompositeService {
+
+    @Autowired
+    private IncomingLegalEntityInvoiceService incomingLegalEntityInvoiceService;
+
+    @Autowired
+    private IncomingInvoiceCertificateService incomingInvoiceCertificateService;
+
+    @Autowired
+    private IncomingInvoiceHideTypeService incomingInvoiceHideTypeService;
+
+    public void removeInvoiceAndCertAndHideTypes(IncomingLegalEntityInvoiceModel invModel, IncomingInvoiceCertificateModel certModel, List<IncomingInvoiceHideTypeModel> hideTypeModels) {
+
+        incomingInvoiceHideTypeService.removeHideTypes(hideTypeModels);
+        incomingInvoiceCertificateService.removeCertificate(certModel);
+        incomingLegalEntityInvoiceService.removeLegalEntityInvoice(invModel);
+
+    }
+
+}
