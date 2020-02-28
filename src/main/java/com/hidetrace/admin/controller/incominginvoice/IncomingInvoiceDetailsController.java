8f86f5f726f083ec32cdcce28ca6b24ac8220613
harisjasar <hare.jasarevic@gmail.com>
2020-02-28 11:22:27 +0100

    added controllers for table view and details view

---
+++ src/main/java/com/hidetrace/admin/controller/incominginvoice/IncomingInvoiceDetailsController.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hidetrace.admin.controller.incominginvoice;
+
+import com.hidetrace.admin.model.CertificateModel;
+import com.hidetrace.admin.model.incominginvoice.IncomingInvoiceCertificateModel;
+import com.hidetrace.admin.model.incominginvoice.IncomingInvoiceHideTypeModel;
+import com.hidetrace.admin.model.incominginvoice.IncomingLegalEntityInvoiceModel;
+import com.hidetrace.admin.service.CertificateService;
+import com.hidetrace.admin.service.incominginvoice.IncomingInvoiceCertificateService;
+import com.hidetrace.admin.service.incominginvoice.IncomingInvoiceHideTypeService;
+import com.hidetrace.admin.service.incominginvoice.IncomingLegalEntityInvoiceService;
+import com.hidetrace.admin.view.incominginvoice.IncomingInvoiceDetailsView;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Jashar
+ */
+@Component
+public class IncomingInvoiceDetailsController {
+
+    @Autowired
+    private IncomingInvoiceDetailsView view;
+
+    @Autowired
+    private IncomingLegalEntityInvoiceService incomingLegalEntityInvoiceService;
+
+    @Autowired
+    private IncomingInvoiceCertificateService incomingInvoiceCertificateService;
+
+    @Autowired
+    private CertificateService certificateService;
+
+    @Autowired
+    private IncomingInvoiceHideTypeService incomingInvoiceHideTypeService;
+
+    private int invoiceId;
+
+    private void initView() {
+        view.setResizable(false);
+        view.setLocationRelativeTo(null);
+        view.setVisible(true);
+
+    }
+
+    private void initListeners() {
+
+    }
+
+    private void initData() {
+        populateInvoiceId();
+    }
+
+    public void start() {
+        System.out.println(this.invoiceId);
+        initListeners();
+        initData();
+        initView();
+
+    }
+
+    public void passInvoiceId(int id) {
+        this.invoiceId = id;
+    }
+
+    private void populateInvoiceId() {
+        IncomingLegalEntityInvoiceModel invoice = incomingLegalEntityInvoiceService.findById(invoiceId).get();
+        if (invoice != null) {
+            view.getIncomingInvoiceIdTextfield().setText(String.valueOf(invoice.getInvId()));
+            view.getInvoiceNameTextField().setText(invoice.getInvName());
+            view.getGrossWeightTextField().setText(String.valueOf(invoice.getInvGrossWeight()));
+            view.getNetWeightTextField().setText(String.valueOf(invoice.getInvNetWeight()));
+            view.getAbroadReducedTextField().setText(String.valueOf(invoice.getInvAbroadReduced()));
+            view.getSaltTextField().setText(String.valueOf(invoice.getInvSalt()));
+            view.getDescriptionTextField().setText(invoice.getInvDescription());
+            view.getDateTimeTextField().setText(String.valueOf(invoice.getInvTimeStamp()));
+            view.getTotalLoadTextField().setText(String.valueOf(invoice.getInvTotalLoad()));
+            view.getDifferenceTextField().setText(String.valueOf(invoice.getInvDifference()));
+
+            IncomingInvoiceCertificateModel certificateModel = incomingInvoiceCertificateService.findByIncomingInvoiceId(invoice.getInvId());
+            view.getCertificateTextField().setText(certificateModel.getCertificateNumber());
+
+            List<CertificateModel> certificateModels = certificateService.findAll();
+            view.getCertificateTypeTextField().setText(certificateModels.get(certificateModel.getCertificateId() - 1).getName());
+
+            List<IncomingInvoiceHideTypeModel> hideTypes = incomingInvoiceHideTypeService.findAllByIncomingInvoiceId(invoice.getInvId());
+            hideTypes.forEach((hideType) -> {
+                switch (hideType.getHideTypeId()) {
+                    case 1:
+                        view.getCowTextField().setText(String.valueOf(hideType.getPrice()));
+                        break;
+                    case 2:
+                        view.getBullTextField().setText(String.valueOf(hideType.getPrice()));
+                        break;
+                    case 3:
+                        view.getCalfTextField().setText(String.valueOf(hideType.getPrice()));
+                        break;
+                    default:
+                        break;
+                }
+            });
+
+        }
+    }
+}
---
+++ src/main/java/com/hidetrace/admin/controller/incominginvoice/IncomingInvoiceInvoicesListController.java
@@ -0,0 +1,152 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hidetrace.admin.controller.incominginvoice;
+
+import com.hidetrace.admin.model.LegalEntityModel;
+import com.hidetrace.admin.model.incominginvoice.IncomingLegalEntityInvoiceModel;
+import com.hidetrace.admin.service.LegalEntityService;
+import com.hidetrace.admin.service.incominginvoice.IncomingLegalEntityInvoiceService;
+import com.hidetrace.admin.view.incominginvoice.IncomingInvoiceInvoicesListView;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTable;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Jashar
+ */
+@Component
+public class IncomingInvoiceInvoicesListController {
+
+    @Autowired
+    private IncomingInvoiceInvoicesListView view;
+
+    @Autowired
+    private ApplicationContext appContext;
+
+    @Autowired
+    private LegalEntityService legalEntityService;
+
+    @Autowired
+    private IncomingLegalEntityInvoiceService incomingLegalEntityInvoiceService;
+
+    private void initView() {
+        view.setResizable(false);
+        view.setLocationRelativeTo(null);
+        view.setVisible(true);
+
+    }
+
+    private void initListeners() {
+        if (view.getLegalEntityDropDown().getActionListeners().length == 0) {
+            view.getLegalEntityDropDown().addActionListener(appContext.getBean(LegalEntityDropDownActionListenerInvoicesListView.class));
+        }
+        if (view.getInvoicesTable().getMouseListeners().length == 2) {
+            view.getInvoicesTable().addMouseListener(appContext.getBean(InvoicesTableMouseSelectionAdapter.class));
+        }
+
+    }
+
+    private void initData() {
+        DefaultComboBoxModel model = (DefaultComboBoxModel) view.getLegalEntityDropDown().getModel();
+        model.removeAllElements();
+
+        ArrayList<LegalEntityModel> entities = (ArrayList<LegalEntityModel>) legalEntityService.getAllLegalEntities();
+        entities.forEach((entity) -> {
+            model.addElement(entity);
+        });
+
+    }
+
+    public void start() {
+        initListeners();
+        initData();
+        initView();
+    }
+
+    @Component
+    private static class LegalEntityDropDownActionListenerInvoicesListView implements ActionListener {
+
+        @Autowired
+        private IncomingInvoiceInvoicesListController controller;
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            controller.populateInvoicesTable();
+
+        }
+
+    }
+
+    @Component
+    private static class InvoicesTableSelectionListener implements ListSelectionListener {
+
+        @Autowired
+        private IncomingInvoiceInvoicesListController controller;
+
+        @Override
+        public void valueChanged(ListSelectionEvent e) {
+            JTable table = controller.getInvoicesTable();
+            int row = table.getSelectedRow();
+            int col = table.getSelectedColumn();
+            int selectedItem = (int) table.getValueAt(row, 0);
+            System.out.println(selectedItem);
+        }
+
+    };
+
+    @Component
+    private static class InvoicesTableMouseSelectionAdapter extends MouseAdapter {
+
+        @Autowired
+        private IncomingInvoiceInvoicesListController controller;
+
+        @Autowired
+        private IncomingInvoiceDetailsController detailsController;
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            JTable table = controller.getInvoicesTable();
+            int row = table.getSelectedRow();
+            int col = table.getSelectedColumn();
+            int selectedItem = (int) table.getValueAt(row, 0);
+            detailsController.passInvoiceId(selectedItem);
+            detailsController.start();
+
+        }
+    }
+
+    private JTable getInvoicesTable() {
+        return view.getInvoicesTable();
+    }
+
+    private void populateInvoicesTable() {
+        LegalEntityModel legalEntityModel = (LegalEntityModel) view.getLegalEntityDropDown().getSelectedItem();
+        if (legalEntityModel != null) {
+            List<IncomingLegalEntityInvoiceModel> invoices = incomingLegalEntityInvoiceService.findByLegalEntityId(legalEntityModel.getLegalEntityID());
+
+            DefaultTableModel invoicesTable = (DefaultTableModel) view.getInvoicesTable().getModel();
+            invoicesTable.setRowCount(0);
+            Object rowData[] = new Object[2];
+            for (int i = 0; i < invoices.size(); i++) {
+                rowData[0] = invoices.get(i).getInvId();
+                rowData[1] = invoices.get(i).getInvName();
+                invoicesTable.addRow(rowData);
+            }
+        }
+    }
+}
