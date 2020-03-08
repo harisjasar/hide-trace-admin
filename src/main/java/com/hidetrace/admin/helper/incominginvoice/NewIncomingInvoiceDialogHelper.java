0adc48b295d8a047f0a823dde7860d708a1eb370
harisjasar <hare.jasarevic@gmail.com>
2020-03-08 23:19:25 +0100

    refactored way we check for articles

--- src/main/java/com/hidetrace/admin/helper/incominginvoice/NewIncomingInvoiceDialogHelper.java
+++ src/main/java/com/hidetrace/admin/helper/incominginvoice/NewIncomingInvoiceDialogHelper.java
@@ -5,10 +5,8 @@
 package com.hidetrace.admin.helper.incominginvoice;
 
 import com.hidetrace.admin.common.MessageDialog;
+import com.hidetrace.admin.common.Validation;
 import com.hidetrace.admin.controller.incominginvoice.NewIncomingInvoiceDialogController;
-import java.util.HashMap;
-import javax.swing.JTextField;
-import javax.swing.JToggleButton;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -25,73 +23,22 @@ public class NewIncomingInvoiceDialogHelper {
     @Autowired
     MessageDialog messageDialog;
 
+    @Autowired
+    Validation validation;
+
     public void addInvoice() {
 
-        boolean emptyField = isFieldEmpty(controller.arrayOfTextFields());
+        boolean emptyField = validation.isFieldEmpty(controller.arrayOfTextFields());
 
         if (emptyField) {
             messageDialog.EmptyFieldForbidden();
         } else {
-            boolean[] articleState = articleState(controller.getArticleTextFields(), controller.getArticleToggleBtns());
-            boolean notTypeSelected = articleState[0];
-            boolean notTypePriceEntered = articleState[1];
 
-            if (notTypeSelected) {
-                if (notTypePriceEntered) {
-                    if (controller.confirmData()) {
-                        if (controller.saveInvoice()) {
-                            controller.completeSave();
-                        }
-                    }
-                } else {
-                    messageDialog.EnterPriceForSelectedType();
-                }
-            } else {
-                messageDialog.SelectType();
-            }
-        }
-
-    }
-
-    /**
-     *
-     * @param txtFields
-     * @return
-     */
-    private boolean isFieldEmpty(JTextField[] txtFields) {
-        boolean field = false;
-        for (JTextField txtField : txtFields) {
-            if (txtField.getText().isEmpty()) {
-                field = true;
-            }
-        }
-
-        return field;
-    }
-
-    private boolean[] articleState(JTextField[] txtFields, JToggleButton[] tBtns) {
-        JTextField[] articleTxtFields = txtFields;
-        JToggleButton[] tglBtns = tBtns;
-
-        HashMap hydeTypeHashMap = new HashMap();
-        for (int i = 0; i < tglBtns.length; i++) {
-            hydeTypeHashMap.put(tglBtns[i], articleTxtFields[i]);
-        }
-
-        boolean notTypeSelected = false;
-        boolean notTypePriceEntered = true;
-        for (JToggleButton tglBtn : tglBtns) {
-            if (tglBtn.isSelected()) {
-                notTypeSelected = true;
-                if (((JTextField) hydeTypeHashMap.get(tglBtn)).getText().isEmpty()) {
-                    notTypePriceEntered = false;
+            if (controller.confirmData()) {
+                if (controller.saveInvoice()) {
+                    controller.completeSave();
                 }
             }
         }
-
-        boolean[] articleState = {notTypeSelected, notTypePriceEntered};
-
-        return articleState;
-
     }
 }
