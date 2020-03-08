a7caa0efd93a47743e5d049c31a135844342475d
harisjasar <hare.jasarevic@gmail.com>
2020-03-08 23:07:08 +0100

    added method saveIncomingInvoiceHideTypeCategory

--- src/main/java/com/hidetrace/admin/service/CompositeService.java
+++ src/main/java/com/hidetrace/admin/service/CompositeService.java
@@ -7,6 +7,7 @@ package com.hidetrace.admin.service;
 
 import com.hidetrace.admin.model.LegalEntityModel;
 import com.hidetrace.admin.model.incominginvoice.IncomingInvoiceCertificateModel;
+import com.hidetrace.admin.model.incominginvoice.IncomingInvoiceHideTypeCategoryModel;
 import com.hidetrace.admin.model.incominginvoice.IncomingInvoiceHideTypeModel;
 import com.hidetrace.admin.model.incominginvoice.IncomingLegalEntityInvoiceModel;
 import com.hidetrace.admin.service.incominginvoice.IncomingInvoiceCertificateService;
@@ -22,7 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
  * @author Jashar
  */
 @Component
-@Transactional
+
 public class CompositeService {
 
     @Autowired
@@ -37,6 +38,10 @@ public class CompositeService {
     @Autowired
     private LegalEntityService legalEntityService;
 
+    @Autowired
+    private IncomingInvoiceHideTypeCategoryService incomingInvoiceHideTypeCategoryService;
+
+    @Transactional
     public void removeInvoiceAndCertAndHideTypes(IncomingLegalEntityInvoiceModel invModel, IncomingInvoiceCertificateModel certModel, List<IncomingInvoiceHideTypeModel> hideTypeModels) {
 
         incomingInvoiceHideTypeService.removeHideTypes(hideTypeModels);
@@ -45,9 +50,34 @@ public class CompositeService {
 
     }
 
+    @Transactional
     public void removeLegalEntityAndLegalEntityTable(LegalEntityModel model, String tableName) {
         legalEntityService.removeLegalEntity(model);
         legalEntityService.deleteLegalEntityTable(tableName);
+    }
+
+    @Transactional
+    public boolean saveIncomingInvoiceHideTypeCategory(IncomingLegalEntityInvoiceModel invoiceModel, List<IncomingInvoiceHideTypeCategoryModel> hideTypesAndCategories, IncomingInvoiceCertificateModel certModel) {
+        try {
+            IncomingLegalEntityInvoiceModel newInvoiceModel = incomingLegalEntityInvoiceService.saveIncomingInvoice(invoiceModel);
+            if (newInvoiceModel != null) {
+                hideTypesAndCategories.forEach((a) -> {
+                    a.setIncomingInvoiceId(newInvoiceModel.getInvId());
+                });
+                incomingInvoiceHideTypeCategoryService.saveAll(hideTypesAndCategories);
+
+                certModel.setIncomingInvoiceId(newInvoiceModel.getInvId());
+                incomingInvoiceCertificateService.saveIncomingInvoiceCertificate(certModel);
+
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+            return false;
+        }
+
     }
 
 }
